fc7407dc841a1b96f44283f457800165acd08c56
Gilles Veyet <34187169+gillesveyet@users.noreply.github.com>
2019-06-01 03:02:21 +0200

    ng generate web-worker base/Solver

--- src/app/base/Solver.ts
+++ src/app/base/Solver.ts
@@ -173,3 +173,15 @@ export class Solver {
         return max;
     }
 } 
+
+if (typeof Worker !== 'undefined') {
+  // Create a new
+  const worker = new Worker('./Solver.worker', { type: 'module' });
+  worker.onmessage = ({ data }) => {
+    console.log(`page got message: ${data}`);
+  };
+  worker.postMessage('hello');
+} else {
+  // Web Workers are not supported in this environment.
+  // You should add a fallback so that your program still executes correctly.
+}
